3ac09238d24480f63a853a5c60caa687f444f763
SEMOELKBER21 <93216241+SEMOELKBER21@users.noreply.github.com>
2022-01-06 05:11:27 +0200

    Update ping.py

--- Music/Plugins/ping.py
+++ Music/Plugins/ping.py
@@ -21,7 +21,7 @@ Disk: {disk}%
     return stats
 
 
-@app.on_message(filters.command("ping"))
+@app.on_message(filters.command(["ping", " السرعه"]))
 async def ping(_, message):
     uptime = await bot_sys_stats()
     start = datetime.now()
@@ -29,5 +29,5 @@ async def ping(_, message):
     end = datetime.now()
     resp = (end - start).microseconds / 1000
     await response.edit(
-        f"**Pong !!**\n`💫{resp} ms`\n\n<b><u>Statistik Sistem Musik:</u></b>{uptime}"
+        f"**Pong !!**\n`💫{resp} ms`\n\n<b><u>إحصائيات نظام الموسيقى:</u></b>{uptime}"
     )
