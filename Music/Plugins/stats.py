30da39c69eb3d1fe908d0f35b8b93c145a530a4d
SEMOELKBER21 <93216241+SEMOELKBER21@users.noreply.github.com>
2022-02-18 18:54:34 +0200

    Update stats.py

--- Music/Plugins/stats.py
+++ Music/Plugins/stats.py
@@ -70,7 +70,7 @@ Ping: `⚡{resp} ms`
 async def stats_markup(_, CallbackQuery):
     command = CallbackQuery.matches[0].group(1)
     if command == "sys_stats":
-        await CallbackQuery.answer("Getting System Stats...", show_alert=True)
+        await CallbackQuery.answer("الحصول على احصائيات النظام", show_alert=True)
         sc = platform.system()
         arch = platform.machine()
         ram = (
